b7cbf34a58fb8bb7a5d8a3b10b2a7c297b986604
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2022-09-25 07:58:20 -0500

    Almost final

--- screens/Camera.js
+++ screens/Camera.js
@@ -1,97 +1,147 @@
 import { Camera } from "expo-camera";
 import { useRef, useState } from "react";
-import { Text, TouchableOpacity} from "react-native";
-import { Icon } from 'react-native-elements'
+import { Text, TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
 import { Box, IconButton } from "@react-native-material/core";
 import { TFJSView } from "./TestPick";
-import * as ImageManipulator from 'expo-image-manipulator'
-import {useNetInfo} from "@react-native-community/netinfo";
-import * as Network from 'expo-network';
+import * as ImageManipulator from "expo-image-manipulator";
+import { useNetInfo } from "@react-native-community/netinfo";
+import * as Network from "expo-network";
 
 export const CameraWrapper = ({ navigation }) => {
-
   const [type, setType] = useState(Camera.Constants.Type.back);
   const cameraRef = useRef(null);
   const [lastPhotoURI, setLastPhotoURI] = useState(null);
-  const [result, setResult] = useState(0)
+  const [result, setResult] = useState(0);
 
   if (lastPhotoURI !== null) {
     return (
       <>
-        <Box width="100%" height="100%" display="flex" flexDirection="row" style={{ backgroundColor: "#2EAF7D" }} pt={60} pl={20}>
-          <IconButton onPress={() => {
-            setLastPhotoURI(null)
-          }} icon={() => (<Icon size="36" color={"white"} name="close"></Icon>)}></IconButton>
-          <Text style={{ color: "white", fontSize: 24, marginTop: 10, marginLeft: 10 }} >Weather Report</Text>
-          <Text>{result < 0.2 ? "SEVERE" : "FAIR"}</Text>
-          <TFJSView result={lastPhotoURI} />
+        <Box
+          width="100%"
+          height="100%"
+          display="flex"
+          flexDirection="column"
+          style={{ backgroundColor: "#2EAF7D" }}
+          pt={60}
+          pl={20}
+        >
+          <Box display="flex" flexDirection="row"> 
+            <IconButton
+              onPress={() => {
+                setLastPhotoURI(null);
+              }}
+              icon={() => <Icon size="36" color={"white"} name="close"></Icon>}
+            ></IconButton>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 24,
+                marginTop: 10,
+                marginLeft: 10,
+              }}
+            >
+              Weather Report
+            </Text>
+          </Box>
+          <TFJSView result={lastPhotoURI} setResult={setResult} />
         </Box>
       </>
     );
   }
 
   return (
-      <Camera style={{ position: "fixed", height: "100%", width: "100%" }} type={type} ref={cameraRef}>
+    <Camera
+      style={{ position: "fixed", height: "100%", width: "100%" }}
+      type={type}
+      ref={cameraRef}
+    >
+      <Box
+        style={{
+          width: "100%",
+          height: "100%",
+          paddingTop: 40,
+          alignItems: "center",
+        }}
+      >
+        <Box
+          style={{
+            width: "100%",
+            height: "20%",
+            padding: 30,
+            alignItems: "left",
+            justifyContent: "top",
+          }}
+        >
+          <IconButton
+            icon={(props) => (
+              <Icon size={48} name="arrow-back" color={"white"}></Icon>
+            )}
+            onPress={() => {
+              navigation.navigate("Home");
+            }}
+            title="back"
+          ></IconButton>
+        </Box>
 
-      <Box style={{ width: "100%", height: "100%", paddingTop: 40, alignItems: "center" }}>
-          <Box style={{ width: "100%", height: "20%", padding: 30, alignItems: "left", justifyContent: "top" }}>
-          <IconButton icon={(props) => (<Icon size={48} name="arrow-back" color={"white"}></Icon>)} onPress={() => {
-              navigation.navigate("Home")
-          }} title="back"></IconButton>
-          </Box>
-          
-          <TouchableOpacity
-          style={{ width: 70,
-              height: 70,
-              justifyContent: 'left',
-              alignItems: 'left',
-              padding: 10,
-              borderRadius: 100,
-              backgroundColor: '#2EAF7D',
-              position: 'absolute',
-              bottom: '10%',
-              left: '46%'}}
-          color = "white"
-          onPress={ () => {
-              Detect({ ref: cameraRef, setLastPhotoURI: setLastPhotoURI })
-          }}>
-              <Icon size="50" name='camera' type='ionicon' color='white'/>
-          </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            width: 70,
+            height: 70,
+            justifyContent: "left",
+            alignItems: "left",
+            padding: 10,
+            borderRadius: 100,
+            backgroundColor: "#2EAF7D",
+            position: "absolute",
+            bottom: "10%",
+            left: "46%",
+          }}
+          color="white"
+          onPress={() => {
+            Detect({ ref: cameraRef, setLastPhotoURI: setLastPhotoURI });
+          }}
+        >
+          <Icon size="50" name="camera" type="ionicon" color="white" />
+        </TouchableOpacity>
       </Box>
-      </Camera>
-  )
-  }
+    </Camera>
+  );
+};
 
-  const Detect = async ({ ref, setLastPhotoURI }) => {
+const Detect = async ({ ref, setLastPhotoURI }) => {
   if (ref.current) {
-      let photo = await ref.current.takePictureAsync({ skipProcessing: true });
-      if ((await Network.getNetworkStateAsync()).isConnected) {
-        //waiting for you 
-        const resizedPhoto = await ImageManipulator.manipulateAsync(
-          photo.uri,
-          [{ resize: { width: 128, height: 128 } }],
-        );
-        setLastPhotoURI(photo.uri)
-        const imgB64 = await FileSystem.readAsStringAsync(resizedPhoto.uri, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-        const imgBuffer = utf8.encode(imgB64);
-    
-        const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ photo: imgBuffer, x: 0, y: 0 })
-        };
-    
-        try {
-          let resp = await fetch('http://10.20.9.94:5000/prediction', requestOptions)
-          let resp2 = await resp.json()
-          console.log(resp2)
-          setResult(resp2[0])
-        } catch (err) {
-          console.log(err)
-        }
-      }
+    let photo = await ref.current.takePictureAsync({ skipProcessing: true });
+    if ((await Network.getNetworkStateAsync()).isConnected) {
+      //waiting for you
+      const resizedPhoto = await ImageManipulator.manipulateAsync(photo.uri, [
+        { resize: { width: 128, height: 128 } },
+      ]);
       setLastPhotoURI(photo.uri);
+      const imgB64 = await FileSystem.readAsStringAsync(resizedPhoto.uri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      const imgBuffer = utf8.encode(imgB64);
+
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ photo: imgBuffer, x: 0, y: 0 }),
+      };
+
+      try {
+        let resp = await fetch(
+          "http://10.20.9.94:5000/prediction",
+          requestOptions
+        );
+        let resp2 = await resp.json();
+        console.log(resp2);
+
+        setResult(resp2[0]);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setLastPhotoURI(photo.uri);
   }
-}
+};
--- screens/TestPick.js
+++ screens/TestPick.js
@@ -1,13 +1,20 @@
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import * as tf from '@tensorflow/tfjs'
-import { useEffect, useState, useRef } from 'react';
-import * as ImagePicker from 'expo-image-picker';
-import * as FileSystem from 'expo-file-system';
-import * as ImageManipulator from 'expo-image-manipulator'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import * as tf from "@tensorflow/tfjs";
+import { useEffect, useState, useRef } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import * as ImageManipulator from "expo-image-manipulator";
 
-export const TFJSView = ({ result }) => {
-  const [state, setState] = useState(false)
-  const [modelState, setModelState] = useState(false)
+export const TFJSView = ({ result, setResult }) => {
+  const [state, setState] = useState(false);
+  const [modelState, setModelState] = useState(false);
   const [image, setImage] = useState(null);
   const [model, setModel] = useState();
   const [resourceIO, setResourceIO] = useState();
@@ -16,14 +23,16 @@ export const TFJSView = ({ result }) => {
 
   useEffect(() => {
     async function prepare() {
-      await tf.ready()
-      setState(true)
+      await tf.ready();
+      setState(true);
 
-      const modelJSON = require('../assets/model/lite.json')
-      const weights = require('../assets/model/lite.bin')
+      const modelJSON = require("../assets/model/lite.json");
+      const weights = require("../assets/model/lite.bin");
 
-      const resourceIO = require('@tensorflow/tfjs-react-native')
-      const model = await tf.loadLayersModel(resourceIO.bundleResourceIO(modelJSON, weights));
+      const resourceIO = require("@tensorflow/tfjs-react-native");
+      const model = await tf.loadLayersModel(
+        resourceIO.bundleResourceIO(modelJSON, weights)
+      );
 
       // model.summary();
       setModelState(true);
@@ -31,38 +40,38 @@ export const TFJSView = ({ result }) => {
       setResourceIO(resourceIO);
     }
     prepare();
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (tf.ready()) {
       predict();
     }
-  }, [resourceIO])
+  }, [resourceIO]);
 
-  const getImageData = async(uri) => {
-    console.log("Pre-processing")
+  const getImageData = async (uri) => {
+    console.log("Pre-processing");
     const imgB64 = await FileSystem.readAsStringAsync(uri, {
       encoding: FileSystem.EncodingType.Base64,
     });
-    let imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
+    let imgBuffer = tf.util.encodeString(imgB64, "base64").buffer;
     let raw = new Uint8Array(imgBuffer);
     let decodeImage = resourceIO.decodeJpeg(raw);
     //let input = tf.cast(decodeImage, 'float16');
-    let input = decodeImage
+    let input = decodeImage;
     input = tf.div(input, 255);
     input = tf.reshape(input, [1, 32, 32, 3]);
-    console.log("Pre-processed")
+    console.log("Pre-processed");
     return input;
-  }
+  };
 
   const predict = async () => {
     try {
       const resizedPhoto = await ImageManipulator.manipulateAsync(
         result,
-        [{ resize: { width: 32, height: 32 } }], 
-        { format: 'jpeg' },
+        [{ resize: { width: 32, height: 32 } }],
+        { format: "jpeg" }
       );
-  
+
       setImage(result);
 
       const data = await getImageData(resizedPhoto.uri);
@@ -71,42 +80,92 @@ export const TFJSView = ({ result }) => {
       console.log(input.shape);
       let initialTime = Date.now();
       const output = model.predict(input);
-      let predictions = Array.from(output.dataSync()); 
+      let predictions = Array.from(output.dataSync());
       let finalTime = Date.now();
       let timeTaken = finalTime - initialTime;
       console.log("TAKEN: " + timeTaken);
-      console.log(predictions)
-      const decision = predictions[0] > predictions[1] ? "Fair" : "Severe"
-      setPrediction(decision)
-  }
-    catch (err) {
+      console.log(predictions);
+      const decision = predictions[0] > predictions[1] ? "Fair" : "Severe";
+      setResult(decision);
+      setPrediction(decision);
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-        }}>
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      <Text>
-        TFJS: {state ? "Ready" : "Waiting"} - Model: {modelState ? "Ready" : "Waiting"}
-      </Text>
-      <Text>
-        Result: {prediction}
-      </Text>
-      <Button title='Predict' onPress={predict} />
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    > {
+    }
+      <View style={{ width: "100%", height: "100%", alignItems: "center" }}>
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
+        <View style={{ marginTop: 40 }}>
+          <Text style={{color: "white", fontSize: 36}}>
+            TFJS: {state ? "Ready" : "Waiting"} - Model:{" "}
+            {modelState ? "Ready" : "Waiting"}
+          </Text>
+          <Text style={{color: "white", fontSize: 36}}>Result: {prediction}</Text>
+          <TouchableOpacity style={{
+            borderWidth:1,
+            borderColor:'black',
+            alignItems:'center',
+            justifyContent:'center',
+            width:100,
+            height:150,
+            backgroundColor:'#fff',
+            borderRadius:50,
+          }} onPress={predict}>
+            <Text style={{color: "white", fontSize: 36}}>Forecast</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pentagon: {
+    backgroundColor: 'transparent'
+  },
+  pentagonInner: {
+    width: 90,
+    borderBottomColor: 'red',
+    borderBottomWidth: 0,
+    borderLeftColor: 'transparent',
+    borderLeftWidth: 18,
+    borderRightColor: 'transparent',
+    borderRightWidth: 18,
+    borderTopColor: 'red',
+    borderTopWidth: 50
+  },
+  pentagonBefore: {
+    position: 'absolute',
+    height: 0,
+    width: 0,
+    top: -35,
+    left: 0,
+    borderStyle: 'solid',
+    borderBottomColor: 'red',
+    borderBottomWidth: 35,
+    borderLeftColor: 'transparent',
+    borderLeftWidth: 45,
+    borderRightColor: 'transparent',
+    borderRightWidth: 45,
+    borderTopWidth: 0,
+    borderTopColor: 'transparent',
+  }
 });
